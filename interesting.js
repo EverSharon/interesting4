fc0669d2aa552410c106a2e0544201cba464a82d
sharon <you@example.com>
2015-12-20 02:21:44 +0800

    you

---
+++ interesting.js
@@ -0,0 +1,6 @@
+xiaoyuanf.addEventListener('click',function(){
+	document.querySelector('#girl').style.background='url(./image/girl.jpg)';})
+xiaoyuans.addEventListener('click',function(){
+	document.querySelector('#girl').style.background='url(./image/night.jpg)';})
+xiaoyuant.addEventListener('click',function(){
+	document.querySelector('#girl').style.background='url(./image/people.jpg)';})
